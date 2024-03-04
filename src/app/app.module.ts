f5b5cc946b31229d466efecb96dd678b9b17900c
FcoMdz <al289539@edu.uaa.mx>
2024-03-03 23:02:00 -0600

    eliminar no usado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { CardComponent } from './components/wiki/card/card.component';
 import { DetailsComponent } from './components/wiki/details/details.component';
 import { HomeComponent } from './components/wiki/home/home.component';
 import { CommonModule } from '@angular/common';
@@ -17,7 +16,6 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     FooterComponent,
     NavbarComponent,
-    CardComponent,
     DetailsComponent,
     HomeComponent
   ],
